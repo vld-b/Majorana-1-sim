d64d59ba9a2c81bb22850337ba0ebaae56d2ef46
vld-b <vladbodea2007@gmail.com>
2026-05-12 18:04:24 +0200

    feat(sim): Add event log to show event messages

--- Assets/Scripts/UIActions.cs
+++ Assets/Scripts/UIActions.cs
@@ -1,10 +1,16 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class UIActions : MonoBehaviour
 {
     public ParticleSystem condensationEffect;
     private ParticleSystem.EmissionModule eModule;
+
+    public VerticalLayoutGroup eventLogVerticalGroup;
+    public GameObject eventLogMessage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,12 +28,32 @@ public class UIActions : MonoBehaviour
     public void StartMajorana()
     {
         eModule.enabled = true;
+
         StartCoroutine("TurnCondensationOff");
+
+        AddEventMessage("Starten des Quantencomputers...");
+
+        StartCoroutine("SendCoolingMessage");
     }
 
     private IEnumerator TurnCondensationOff()
     {
         yield return new WaitForSeconds(10);
         eModule.enabled = false;
+
+        AddEventMessage("Kühlung abgeschlossen");
+    }
+
+    private IEnumerator SendCoolingMessage()
+    {
+        yield return new WaitForSeconds(1);
+
+        AddEventMessage("Kühlen des Quantencomputers auf 4mK...");
+    }
+
+    private void AddEventMessage(string message)
+    {
+        GameObject newMessage = Instantiate(eventLogMessage, eventLogVerticalGroup.transform);
+        newMessage.GetComponent<TextMeshProUGUI>().text = message;
     }
 }
