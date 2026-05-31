1d08f5ce700d95bee3910007a1f89f0f040794b6
vld-b <vladbodea2007@gmail.com>
2026-06-01 01:23:46 +0200

    feat(sim): Add text when hovering over quantum chip with mouse, adjust text styles and add even log label

--- Assets/Scripts/Majorana1.cs
+++ Assets/Scripts/Majorana1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 
 public class Majorana1 : MonoBehaviour
@@ -6,15 +8,29 @@ public class Majorana1 : MonoBehaviour
     // List is populated in Unity
     public System.Collections.Generic.List<QBit> qBits;
 
+    public GameObject hoveringTextObject;
+    private TextMeshProUGUI hoveringText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hoveringText = hoveringTextObject.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseEnter()
+    {
+        hoveringText.text = "Quantenchip";
+        hoveringTextObject.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        hoveringTextObject.SetActive(false);
     }
 }
