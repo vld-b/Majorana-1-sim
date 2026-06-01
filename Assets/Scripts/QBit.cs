9bd0badecf4258e67189970f3b6892a10567f3c3
vld-b <vladbodea2007@gmail.com>
2026-06-01 02:22:23 +0200

    feat(sim): Add state vector view on the bottom right

--- Assets/Scripts/QBit.cs
+++ Assets/Scripts/QBit.cs
@@ -1,12 +1,18 @@
+using TMPro;
 using UnityEngine;
 
 public class QBit : MonoBehaviour
 {
-    // Is set in Unity
+    // Are set in Unity
     public RectTransform arrowTransform;
+    public TextMeshProUGUI superpositionLabel;
 
     public bool isStateKnown { get; private set; } = false;
     public float currentAngle { get; private set; } = 0.0f;
+    public float superposition
+    {
+        get => Mathf.Sin(Mathf.Deg2Rad * currentAngle);
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,6 +32,8 @@ public class QBit : MonoBehaviour
     public void SetSuperposition(float angle)
     {
         currentAngle = angle;
+        string superpositionString = (Mathf.Round(superposition * 100.0f) / 100.0f).ToString();
+        superpositionLabel.text = superpositionString;
         arrowTransform.rotation = Quaternion.Euler(0, 0, angle - 90.0f);
     }
 }
